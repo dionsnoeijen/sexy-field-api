8467e72b1b30dd3e2115133753751fdfad4dac3b
Paulo <p.moreno@ltp.nl>
2017-11-22 13:57:38 +0100

    Api/Serializer/FieldsExclusionStrategyTest

---
+++ test/unit/SectionField/Api/Serializer/FieldsExclusionStrategyTest.php
@@ -0,0 +1,58 @@
+<?php
+declare (strict_types=1);
+
+namespace Tardigrades\SectionField\Api\Serializer;
+
+use JMS\Serializer\Metadata\ClassMetadata;
+use JMS\Serializer\Metadata\PropertyMetadata;
+use PHPUnit\Framework\TestCase;
+use JMS\Serializer\SerializationContext;
+
+/**
+ * @coversDefaultClass Tardigrades\SectionField\Api\Serializer\FieldsExclusionStrategy
+ * @covers ::<private>
+ */
+class FieldsExclusionStrategyTest extends TestCase
+{
+    /**
+     * @test
+     * @covers ::__construct
+     */
+    public function it_constructs()
+    {
+        $fields = ['a', 'b', 'c'];
+        $strategy = new FieldsExclusionStrategy($fields);
+        $this->assertInstanceOf(FieldsExclusionStrategy::class, $strategy);
+    }
+
+    /**
+     * @test
+     * @covers ::shouldSkipClass
+     */
+    public function it_should_return_false_for_skip_class()
+    {
+        $strategy = new FieldsExclusionStrategy(['strategic']);
+        $class = \Mockery::mock(ClassMetadata::class)->makePartial();
+        $this->assertFalse($strategy->shouldSkipClass($class, SerializationContext::create()));
+    }
+
+    /**
+     * @test
+     * @covers ::shouldSkipProperty
+     */
+    public function it_should_return_correctly_for_skip_property()
+    {
+        $strategy = new FieldsExclusionStrategy(['apples', 'bananas', 'crustaceans']);
+
+        $property = \Mockery::mock(PropertyMetadata::class);
+        $property->serializedName = false;
+        $property->name = 'bananas';
+
+        $propertytwo = \Mockery::mock(PropertyMetadata::class);
+        $propertytwo->serializedName = false;
+        $propertytwo->name = 'dinosaurs';
+
+        $this->assertFalse($strategy->shouldSkipProperty($property, SerializationContext::create()));
+        $this->assertTrue($strategy->shouldSkipProperty($propertytwo, SerializationContext::create()));
+    }
+}
