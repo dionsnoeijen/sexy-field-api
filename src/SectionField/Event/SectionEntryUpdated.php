a3ecfe366cec966d784fe4df5e39e6720e919220
Marieke Bednarczyk <mbed@live.nl>
2018-04-03 12:58:00 +0200

    Dispatch event when updating
    When updating we want to know what the state of the original entity was so that we can trigger sequential actions based on the updating of one field.
    A new event SectionEntryUpdated is introduced which has both the original as the new event so that they can be compared.

---
+++ src/SectionField/Event/SectionEntryUpdated.php
@@ -0,0 +1,57 @@
+<?php
+
+/*
+ * This file is part of the SexyField package.
+ *
+ * (c) Dion Snoeijen <hallo@dionsnoeijen.nl>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+declare (strict_types = 1);
+
+namespace Tardigrades\SectionField\Event;
+
+use Symfony\Component\EventDispatcher\Event;
+use Tardigrades\SectionField\Generator\CommonSectionInterface;
+
+/**
+ * Class SectionEntryUpdated
+ *
+ * Dispatched after updated section is saved.
+ *
+ * @package Tardigrades\SectionField\Event
+ */
+class SectionEntryUpdated extends Event
+{
+    const NAME = 'section.entry.updated';
+
+    /** @var CommonSectionInterface */
+    protected $originalEntry;
+
+    /** @var CommonSectionInterface */
+    protected $newEntry;
+
+    public function __construct(CommonSectionInterface $originalEntry, CommonSectionInterface $newEntry)
+    {
+        $this->originalEntry = $originalEntry;
+        $this->newEntry = $newEntry;
+    }
+
+    /**
+     * The Section Entry Entity that was just persisted
+     */
+    public function getOriginalEntry(): CommonSectionInterface
+    {
+        return $this->originalEntry;
+    }
+
+    /**
+     * The Section Entry Entity that was just persisted
+     */
+    public function getNewEntry(): CommonSectionInterface
+    {
+        return $this->newEntry;
+    }
+}
