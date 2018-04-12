69f383400972d0dbb7f8ddab35d2ea5f7e6886f3
Jan Verbeek <j.verbeek@ltp.nl>
2018-04-12 12:50:07 +0200

    Use strpos properly

--- src/SectionField/Api/Controller/RestController.php
+++ src/SectionField/Api/Controller/RestController.php
@@ -839,7 +839,7 @@ class RestController implements RestControllerInterface
                     !empty($sexyFieldInstructions['field']) &&
                     !empty($sexyFieldInstructions['value'])
                 ) {
-                    if (strpos(',', $sexyFieldInstructions['value'])) {
+                    if (strpos(',', $sexyFieldInstructions['value']) !== false) {
                         $sexyFieldInstructions['value'] = explode(',', $sexyFieldInstructions['value']);
                     }
                     $readOptions[ReadOptions::FIELD] = [$sexyFieldInstructions['field'] => $sexyFieldInstructions['value']];
